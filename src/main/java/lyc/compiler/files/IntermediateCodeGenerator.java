7916128bd71cefdb4739bb3f1137cdd7a703f3b8
Mauro <maorlandi@alumno.unlam.edu.ar>
2022-10-27 23:53:41 -0700

    Correcciones y mas implementacion de GCI

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -27,15 +27,23 @@ public class IntermediateCodeGenerator implements FileGenerator {
         }
     }
 
+    //Solamente apila en el stack la posicion actual
     public static void apilar(){
+        if(__logmsg){ System.out.println("Apilando: | contenido: " + polaca.size()); }
+        stack.push(polaca.size());
+    }
+
+    //Apila en el stack y deja un lugar vacio
+    public static void apilar_y_avanzar(){
         if(__logmsg){ System.out.println("Apilando: | contenido: " + polaca.size()); }
         stack.push(polaca.size());
         ++celda_actual;
     }
 
-    public static void desapilar(){
+    //El offset indica cuantas celdas tengo que sumar a la celda_actual para hacer el brach de manera correcta
+    public static void desapilar_e_insertar(int offset){
         Integer pos = stack.pop();
-        Integer val = celda_actual + 1;
+        Integer val = celda_actual + offset;
         if(__logmsg){ System.out.println("Desapilando | posicion: " + pos + " valor: " + val); }
         if(pos > polaca.size()){
             polaca.add(val.toString());
@@ -43,6 +51,10 @@ public class IntermediateCodeGenerator implements FileGenerator {
         else{
             polaca.add(pos,val.toString());
         }
+    }
+
+    public static Integer obtener_tope_de_pila(){
+        return stack.pop();
     }
 
     @Override
